2224da92602e863117e1733a82baa613571465bc
Wyatt Greenway <th317erd@gmail.com>
2018-04-17 18:47:18 -0700

    Initial project structure

---
+++ javascript/game.js
@@ -0,0 +1,10 @@
+ktExport('game.js', () => {
+  class Game {
+    // Here we will define game rules
+    // A game will also hold some number of cards and players
+  }
+
+  return {
+    Game
+  };
+});
---
+++ javascript/player.js
@@ -0,0 +1,10 @@
+ktExport('player.js', () => {
+  class Player {
+    // Here we will define a player
+    // Having a structure like this will help with game rules and permissions (i.e. turns)
+  }
+
+  return {
+    Player
+  };
+});
