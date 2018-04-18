1ace0e1e15af1a25c242c281d12cd966ae5622db
Wyatt Greenway <th317erd@gmail.com>
2018-04-17 20:00:47 -0700

    Minor Updates

--- javascript/card.js
+++ javascript/card.js
@@ -1,8 +1,11 @@
 ktExport('card.js', () => {
-  class Card {
-    // Define a generic card and its functionality here
+  // Defining a class
+  class Class {
+    // here we need something to instantiate a single card
   }
 
+  // Defining and returning a simple object
+  // With a single key: Card
   return {
     Card
   };
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,9 @@
 ktExport('main.js', ({ Game }) => {
-  console.log('CARD CLASS: ', Game);
+  console.log('KingTut: ', Game);
+
+  // Empty exports (don't export anything)
+  // An empty object is needed
+  // Because "nothing" (undefined)
+  // would throw an exception
   return {};
 });
