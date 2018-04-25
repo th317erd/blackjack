1b793a5c26d38dad1ba7c46697ac547889749a1f
Mason Ellwood <masonellwood@gmail.com>
2018-04-24 18:12:53 -0700

    initial commit

--- javascript/card.js
+++ javascript/card.js
@@ -1,8 +1,31 @@
 ktExport('card.js', () => {
+  //create class of Cards
   class Card {
-    // Define a generic card and its functionality here
+    //assign getters and setters within the class for the number suit and entire deck
+    constructor(number, suit, deck) {
+      //define the number
+      this.number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+      //define the suit
+      this.suit = ["Hearts", "Dimonds", "Clubs", "Spades"];
+      //define the deck
+      this.deck = [52];
+      //create new object of a builtDeck which is empty
+      var builtDeck = new Object();
+    }
+
+    //define counter_one and counter_two for the exterior and interior loops
+    var counter_one, counter_two;
+    //itterate though all the cards
+    for (counter_one = 0; counter_one < Card.deck[0]; counter_one++ ) {
+      //itterate though all the suits
+      for (counter_two = 0; counter_two < Card.suit.length; counter_two++ ) {
+        // assign suit to a card.number
+        // push those new formed cards into the builtDeck in key value pairs
+      }
+    }
   }
 
+  //log new key value pairs to the console
   return {
     Card
   };
--- javascript/player.js
+++ javascript/player.js
@@ -2,6 +2,10 @@ ktExport('player.js', () => {
   class Player {
     // Here we will define a player
     // Having a structure like this will help with game rules and permissions (i.e. turns)
+
+    // define player
+
+    // allow card to be associated to a 
   }
 
   return {
