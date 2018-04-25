bc46a44562843b2c0429cff561ddfeac9e5e6c1b
chuck <chuckmorrissette@gmail.com>
2018-04-24 20:07:25 -0700

    defining suits

--- javascript/card.js
+++ javascript/card.js
@@ -4,6 +4,88 @@ ktExport('card.js', () => {
     Recomendation: ratios from 0.0 - 1.0 would enable patterns
     to be properly applied to cards of any size
   */
+  /*X would be 0.5 and Y 1.0 the shape of a rectangle.
+   Set up the parameters for the edge of the Box. 
+   set the postions of the patterns in side the paramater for each suit for each card.
+   by setting a different parameters for each card number.
+  */
+
+  const SUITS = {
+    0: {
+      pattern: [
+        { x: 0.0, y: 0.0}
+      ]
+    },   
+    1: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    2: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    3: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    4: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    5: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    6: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    7: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    8: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    9: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    10: {
+      pattern: [
+        {x: 0.0, y: 0.0}
+      ]
+    },
+    11: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    12: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    },
+    13: {
+      pattern: [
+        { x: 0.0, y: 0.0 }
+      ]
+    }
+  };
+    
+ 
+   
+
 
   // Defining a class
   class Card {
