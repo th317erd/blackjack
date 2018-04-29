522ee277033e61c5e00468117a06be3579149f4c
chuck <chuckmorrissette@gmail.com>
2018-04-28 19:03:39 -0700

    update cards

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ ktExport('main.js', ({ Game, Card, renderCard }) => {
 
   // First argument is type, can be one of:
   // diamond, heart, club, or spade
-  renderCard('spade', Card.SUITS[2]);
+  renderCard('spade', Card.SUITS[5]);
 
   // Empty exports (don't export anything)
   // An empty object is needed
