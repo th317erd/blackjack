79506349efcd011b900c8e7dcfed8492e7637fbf
chuck <chuckmorrissette@gmail.com>
2018-05-01 18:17:55 -0700

    fix merge conflicts

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,7 @@ ktExport('main.js', ({ Game, Card, renderCard }) => {
 
   // First argument is type, can be one of:
   // diamond, heart, club, or spade
-<<<<<<< HEAD
   renderCard('spade', Card.SUITS[10]);
-=======
-  renderCard('spade', Card.SUITS[0]);
->>>>>>> b96e9bb80c7390f46ad3a0a98fd2b3c5b59e3cc2
 
   // Empty exports (don't export anything)
   // An empty object is needed
