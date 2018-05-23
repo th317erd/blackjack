c2e7fd2a218ca561c974b70bd3e07106e9232a7d
Wyatt Greenway <th317erd@gmail.com>
2018-05-22 18:20:45 -0700

    Working with webpack and dust for templates

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,5 +6,29 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public', 'javascript'),
     publicPath: '/javascript'
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'client/templates'),
+      'node_modules'
+    ],
+    extensions: [
+      '.js',
+      '.dust'
+    ],
+    alias: { dustjs: 'dustjs-linkedin' }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.dust$/,
+        exclude: /node_modules/,
+        loader: path.resolve( __dirname, 'index' ),
+        options: {
+          root: 'client/templates',
+          verbose: true
+        }
+      }
+    ]
   }
 };
