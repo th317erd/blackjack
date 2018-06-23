ccdbe1ee2c03fe83f26a1680a093e051a180d20e
fyrepaul <paul@fyresite.com>
2018-06-22 20:28:03 -0700

    iscardvisibleto

--- client/card.js
+++ client/card.js
@@ -172,12 +172,16 @@ class Card {
   constructor(value, suit) {
     if (!CARDS.hasOwnProperty(value))
       throw new Error(`Invalid card value: ${value}`);
+    
+      // create variable to hold value
+      var _viewableByPlayers = [];
 
     attrGetterSetter(this, 'value', () => value);
     attrGetterSetter(this, 'suit', () => suit);
     attrGetterSetter(this, 'digit', () => CARDS[value].digit);
     attrGetterSetter(this, 'pattern', () => CARDS[value].pattern);
     attrGetterSetter(this, 'suit-font', () => DEFAULT_SUIT_FONT);
+    attrGetterSetter(this, 'viewableByPlayers', () => _viewableByPlayers );
 
     this.ownerID = 0;
   }
@@ -185,6 +189,39 @@ class Card {
   setOwner(player) {
     this.ownerID = player.id;
   }
+  isVisibleTo(player, set){
+    if(!player) 
+      return false;
+
+    var viewableByPlayers = this.viewableByPlayers;
+    var playerID = player.id;
+
+    // if false NOT VISIBLE
+    if(set === undefined){
+      // GET
+      // step through and check if the ID matches the array of visible to players
+      return (viewableByPlayers.indexOf(playerID) >= 0);
+      // same as above
+      // for ( var i = 0; i < viewableByPlayers.length; i++ ){
+      //   if (playerID === viewableByPlayers[i])
+      //     return true;
+      // }
+
+    } else {
+      // SET
+      if(set === true){
+        viewableByPlayers.push(playerID);
+      } else {
+        //remove player from array
+        var playerIndex = viewableByPlayers.indexOf(playerID);
+        if(playerIndex >= 0){
+          //remove this player from array
+          viewableByPlayers.splice(playerIndex, 1);
+        }
+      }
+    }
+  }
+
 }
 
 Card.CARDS = CARDS;
